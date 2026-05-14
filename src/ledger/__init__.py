85c73539284dea6805b47e18fa281b00eb16f43b
Rahn-Grey <rahnli@foxmail.com>
2026-05-14 13:14:09 +0800

    fix: MOCK_NODE_URLS 环境变量控制远程连接; docker-compose.prod_sim.yml 覆盖文件

--- src/ledger/__init__.py
+++ src/ledger/__init__.py
@@ -13,21 +13,21 @@ from src.config import IS_DEMO, IS_PRODUCTION_SIM, IS_PRODUCTION, CONSENSUS_NODE
 
 logger = logging.getLogger(__name__)
 
-if IS_DEMO:
+# 如果设置了 MOCK_NODE_URLS，强制使用 HTTP 远程客户端
+_mock_urls = os.environ.get("MOCK_NODE_URLS")
+if _mock_urls:
+    from src.debug.mock_bcos_client import MockBCOSClient
+    _urls = [u.strip() for u in _mock_urls.split(",")]
+    BCOSClient = lambda: MockBCOSClient(node_urls=_urls)
+    logger.info("联盟链: MockBCOS HTTP 远程 (%d 独立节点容器)", len(_urls))
+
+elif IS_DEMO:
     from src.debug.mock_bcos import MockBCOS as BCOSClient
     logger.info("联盟链: MockBCOS 进程内 (%d 节点模拟 PBFT)", CONSENSUS_NODE_COUNT)
 
 elif IS_PRODUCTION_SIM:
-    # 如果设置了 MOCK_NODE_URLS 则连接独立容器，否则退回进程内 MockBCOS
-    _mock_urls = os.environ.get("MOCK_NODE_URLS")
-    if _mock_urls:
-        from src.debug.mock_bcos_client import MockBCOSClient
-        _urls = [u.strip() for u in _mock_urls.split(",")]
-        BCOSClient = lambda: MockBCOSClient(node_urls=_urls)
-        logger.info("联盟链: MockBCOS HTTP 远程 (%d 独立节点容器)", len(_urls))
-    else:
-        from src.debug.mock_bcos import MockBCOS as BCOSClient
-        logger.info("联盟链: MockBCOS 进程内 (生产模拟模式, 未配置远程节点)")
+    from src.debug.mock_bcos import MockBCOS as BCOSClient
+    logger.info("联盟链: MockBCOS 进程内 (生产模拟, 未配置远程节点)")
 
 elif IS_PRODUCTION:
     from src.ledger.bcos_client import BCOSClient
